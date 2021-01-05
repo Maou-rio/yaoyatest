b0bcf792802c30e6ba31138bd125ca51d93a313e
rio <rio052787@gmail.com>
2021-01-05 22:50:19 +0900

    create base

---
+++ yaoya.rb
@@ -0,0 +1,28 @@
+vegetables = [
+  { name: "トマト", price: 100},
+  { name: "きゅうり", price: 200},
+  { name: "玉ねぎ", price: 300},
+  { name: "なす", price: 400}
+
+]
+puts "いらっしゃいませ！商品を選んで下さい。
+
+1.トマト（100円）
+2.きゅうり（200円）
+3.玉ねぎ（300円）
+4.なす（400円）
+"
+
+puts"商品の番号を選択 > 1"
+
+
+puts"トマトですね。何個買いますか？"
+
+puts"個数を入力 > 5"
+
+
+
+
+#vegetables.each.with_index(1) do |name,i,price|
+  #puts"#{i}.  #{name}(#{price})"
+#end
